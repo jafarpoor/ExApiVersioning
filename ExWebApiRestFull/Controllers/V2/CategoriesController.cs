7400fce1b6cbc563eed812774f8e1db15ff9f515
unknown <faezehjafarpour75@gmail.com>
2022-02-08 23:00:36 +0330

    versioning

---
+++ ExWebApiRestFull/Controllers/V2/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using ExWebApiRestFull.Model.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExWebApiRestFull.Controllers.V2
+{
+    [ApiVersion("2")]
+    // توس تست سوگر برای ورژن 1 کار نمی کنه چونکه یو ار الش متفاوته
+    [Route("api/v{version:apiversion}/[controller]")]
+    [ApiController]
+    public class CategoriesController : V1.ExWebApiRestFull.Controllers.CategoriesController
+    {
+        private readonly CategoryRepository categoryRepository;
+        public CategoriesController(CategoryRepository category) : base(category)
+        {
+            categoryRepository = category;
+        }
+        [HttpGet]
+        public override IActionResult Get()
+        {
+            string[] mystring = { "value1", "value2", "value3" };
+            return Ok(mystring);
+        }
+    }
+}
